ea2f804f6a54a1d8930cd3ce8ba477b5ea7fb562
josedhonatas <dhonatas@academico.ufs.br>
2023-09-12 23:04:32 -0300

    insercao-infra

---
+++ src/main/java/com/example/crud/infra/RequestsExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.crud.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestsExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(){  //dado nao encontrado
+        return ResponseEntity.badRequest().body("Dado nao encontrado");
+    }
+}
